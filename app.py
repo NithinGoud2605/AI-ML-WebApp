30ed97c2371449e193685372ecbbeb4518db07d3
NithinGoud2605 <sainithingoud007@gmail.com>
2024-12-02 23:22:45 -0800

    add

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import pyodbc
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+# Database connection string
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=retail-sql-server-cc.database.windows.net;'
+    'DATABASE=Userdatabase;'
+    'UID=NithinGoud;'
+    'PWD=password@123'
+)
 
 @app.route('/')
 def home():
@@ -11,7 +21,16 @@ def register():
     username = request.form['username']
     password = request.form['password']
     email = request.form['email']
-    return f"User {username} registered successfully with email {email}!"
+
+    # Insert user into database
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO users (username, password, email)
+        VALUES (?, ?, ?)
+    """, (username, password, email))
+    conn.commit()
+
+    return f"User {username} registered successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
